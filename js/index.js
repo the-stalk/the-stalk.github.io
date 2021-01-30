a1ae8c4204e888938e77e2afb3a401a46941e7f4
Miranda Elliott <melliott@v4development.com>
2021-01-29 19:44:09 -0500

    added cassette and recent review link to header

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
+
+// const wavetext = document.querySelector('.wavetext')
+
+// const text = '"what previously felt confrontational is now rendered intangible and strange, an echo of a previous thought or action left to float high up around an empty room"';
+// console.log(text)
+
+// for(var i in text) {
+// 	var span = document.createElement('span')
+
+//   if(text[i] === " ") {
+//   	span.innerHTML = '&nbsp;'
+//   	// node = document.createTextNode('&nbsp;');
+
+//     // wavetext.append(document.createElement('span').html("&nbsp;"))
+//   } else {
+//     // wavetext.append(document.createElement('span').text(text[i]))
+//     node = document.createTextNode(text[i])
+//     span.appendChild(node)
+//   }
+
+//   wavetext.appendChild(span)
+// }
